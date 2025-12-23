7c0fe55c42e9ebb14f82f6f5608b7e91eea6678e
Dulidanci <dulacska.daniel@gmail.com>
2025-12-23 11:56:31 +0100

    Snake and TurboSnake

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,25 @@ public class Main {
         for (int i = 1; i <= 8; i++) {
             System.out.println("i = " + i);
         }
+
+        Snake kigyo = new Snake(1, 4);
+        Snake kigyo2 = new Snake(-10, 1);
+
+        TurboSnake kigyoTurbo = new TurboSnake(5, 3);
+        kigyoTurbo.multiplier = 5;
+
+        System.out.println(kigyoTurbo.result());
+
+        if (kigyo instanceof TurboSnake turboSnake) {
+            turboSnake.multiplier = 5;
+        }
+
+
+        Snake.setK('{');
+        System.out.println(Snake.getK());
+
+        kigyo.step();
+
+        System.out.println(kigyo2.result());
     }
 }
---
+++ src/TurboSnake.java
@@ -0,0 +1,21 @@
+public class TurboSnake extends Snake {
+    public int multiplier;
+
+    public TurboSnake(int position, int length, int multiplier) {
+        super(position, length);
+        this.multiplier = multiplier;
+    }
+
+    public TurboSnake(int length, int multiplier) {
+        super(length);
+        this.multiplier = multiplier;
+    }
+
+    @Override
+    public void step() {
+        while (length > 0) {
+            length--;
+            position += multiplier;
+        }
+    }
+}
